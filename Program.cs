fea372a0ad389b41566ee715a9a207479d02542e
prince475 <danielmwai475@gmail.com>
2023-05-26 00:33:51 +0300

    Performing Bank Transactions like depositing and withdrawals.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var account = new BankAccount("Bree", 10000);
+           Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+           account.MakeWithdrawal(1200, DateTime.Now, "Ring");
+           // Console.WriteLine(account.Balance);
+           account.MakeWithdrawal(500, DateTime.Now, "Flowers");
+           // Console.WriteLine(account.Balance);
+
+           // Account history
+
+           Console.WriteLine(account.GetAccountHistory());
+
+
+           // Catching an Exception
+           // Testing the initial balance must be positive
+
+        //    try
+        //    {
+        //         var invalidAccount = new BankAccount("Invalid", -560);
+        //    }
+        //    catch (ArgumentOutOfRangeException e)
+        //    {
+        //     Console.WriteLine("Exception caught creating account with negative balance");
+        //     Console.WriteLine(e.ToString());
+        //    }
+
+           // Testing for negative balances overdraws
+
+        //    try
+        //    {
+        //         account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw the account");
+        //    }
+        //    catch (InvalidOperationException e)
+        //    {
+        //     Console.WriteLine("Exception caught trying to overdraw the account");
+        //     Console.WriteLine(e.ToString());
+        //    }
+
+
+
+
+        }
+    }
+}
