c888d540154dbe1fc35e02687f17a6e6d81afbed
prince475 <danielmwai475@gmail.com>
2023-05-26 00:33:04 +0300

    Adding new transactions to our ban account

---
+++ Transaction.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MySuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount { get;}
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction (decimal amount, DateTime date, string notes)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = notes;
+        }
+    }
+}
